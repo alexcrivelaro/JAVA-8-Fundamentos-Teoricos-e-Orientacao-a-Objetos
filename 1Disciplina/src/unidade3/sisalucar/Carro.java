f20a984aa60902f7ffdc4b622c80fdd02602bbe2
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2018-06-19 10:52:57 -0300

    Atributos Constantes, Enumeracoes e Metodos Constantes

--- 1Disciplina/src/unidade3/sisalucar/Carro.java
+++ 1Disciplina/src/unidade3/sisalucar/Carro.java
@@ -1,6 +1,7 @@
 /*------------------------------------*\
- Criação dos atributos da classe carro -------- Aula 41
- Criação dos construtores    
+ Criação dos atributos da classe carro 
+ Criação dos construtores 
+ Adição do atributo Enum Revendedor   
 \*------------------------------------*/
 
 package unidade3.sisalucar;
@@ -14,8 +15,10 @@ public class Carro {
 	int ano;
 	String cor;
 	float valorDiaria;
+	Revendedor revendedor;
 	
-	public Carro(long idCarro, String placa, String fabricate, String modelo, int ano, String cor, float valorDiaria) {
+	public Carro(long idCarro, String placa, String fabricate, String modelo, 
+			int ano, String cor, float valorDiaria, Revendedor revendedor) {
 		super(); // chama o construtor da super classe
 		
 		this.idCarro = idCarro;  // <this.idCarro> é o ATRIBUTO que vai receber a VARIÁVEL <idCarro>
@@ -25,13 +28,14 @@ public class Carro {
 		this.ano = ano;
 		this.cor = cor;
 		this.valorDiaria = valorDiaria;
+		this.revendedor = revendedor;
 	}
 	
 	public Carro() {  // criar o construtor vazio para receber objetos vazios!!!
 	}
 	
 	public static void main(String[] args) {
-		Carro carro1 = new Carro(1, "ABC-2001", "Volks", "Gol", 2013, "azul", 99.90f); 
+		Carro carro1 = new Carro(1, "ABC-2001", "Volks", "Gol", 2013, "azul", 99.90f, Revendedor.EMANUELVEICULOS); 
 		Carro carro2 = new Carro();   // objeto vazio
 	}
 }
